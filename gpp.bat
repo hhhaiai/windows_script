053f513def738a03bda3db6dca698e42f3319671
sanbo <sanbo.xyz@gmail.com>
2025-07-25 23:24:42 +0800

    update 2025/07/25 周五 23:24:41.64

--- gpp.bat
+++ gpp.bat
@@ -13,7 +13,7 @@ git diff --quiet --exit-code || (
 git rev-parse --abbrev-ref @{u} >nul 2>&1 && (
     git push 2>&1 | find "Everything up-to-date" >nul || (git push && echo Push completed)
 ) || (
-    git push -u origin HEAD && echo First push completed
+    git push
 )
 
 echo Operation completed!
